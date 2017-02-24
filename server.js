e2fd791e6e32da18dbeea0a058ea6e7f6557c313
shantanu2010 <shantanu4644@gmail.com>
2017-02-24 18:11:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.get('/:articleName',function(req , res){
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/:article-two',function(req , res){
+/* app.get('/:article-two',function(req , res){
     
     res.send(createTemplate(articleTwo));
 });
@@ -145,7 +145,7 @@ app.get('/:article-three',function(req , res){
     
     res.send(createTemplate(articleThree));
 });
-
+*/
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
