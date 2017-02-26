722461b429588db0b605c31276ec64a884acea34
shantanu2010 <shantanu4644@gmail.com>
2017-02-26 11:22:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,6 +136,13 @@ app.get('/:articleName',function(req , res){
   res.send(createTemplate(articles[articleName]));
 });
 
+var  counter = 0;
+app.get('/counter' ,function(req,res){
+    
+   counter = counter + 1;
+    res.send(counter.toString());   
+});
+
 /* app.get('/:article-two',function(req , res){
     
     res.send(createTemplate(articleTwo));
